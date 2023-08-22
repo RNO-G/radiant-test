1367083ff80aa97b490fa56e7a2baae164cd5040
lpyras <pyras@posteo.de>
2023-08-22 14:09:44 +0200

    add test AUXTriggerResponse

---
+++ radiant_test/AWG4022.py
@@ -0,0 +1,141 @@
+import logging
+import math
+import numpy as np
+import struct
+import json
+import vxi11
+
+from .AbstractSignalGenerator import AbstractSignalGenerator
+
+
+def validate_channel(func):
+    def inner(self, *args, **kwargs):
+        allowed_channels = [1, 2]
+
+        if len(args) > 0:
+            if args[0] not in allowed_channels:
+                raise ValueError(f"Channel {args[0]} not supported.")
+        else:
+            if "channel" not in kwargs:
+                raise ValueError("No channel specified.")
+            if kwargs["channel"] not in allowed_channels:
+                raise ValueError(f"Channel {kwargs['channel']} not supported.")
+        func(self, *args, **kwargs)
+
+    return inner
+
+
+class AWG4022(AbstractSignalGenerator):
+    def __init__(self, ip_address):
+        self.instrument = vxi11.Instrument(ip_address)
+        logging.debug(f"Connected to {self.get_id()} at address {ip_address}.")
+
+    def get_id(self):
+        return self.instrument.ask("*IDN?")
+
+    @validate_channel
+    def output_off(self, channel):
+        self.instrument.write(f"OUTP{channel} OFF")
+
+    @validate_channel
+    def output_on(self, channel):
+        self.instrument.write(f"OUTP{channel} ON")
+
+    def run_instrument(self):
+        self.instrument.write("AFGControl:START")
+
+    @validate_channel
+    def set_amplitude_mVpp(self, channel, amplitude):
+        AMPLITUDE_MIN = 50
+        AMPLITUDE_MAX = 1200
+
+        if amplitude < AMPLITUDE_MIN or amplitude > AMPLITUDE_MAX:
+            raise ValueError(
+                f"Only accepting values of {AMPLITUDE_MIN} <= amplitude <= {AMPLITUDE_MAX}."
+            )
+        self.instrument.write(f"SOUR{channel}:VOLT:UNIT VPP")
+        self.instrument.write(f"SOUR{channel}:VOLT:AMPL {amplitude*1e-3}")
+
+    @validate_channel
+    def set_frequency_MHz(self, channel, frequency):
+        self.instrument.write(f"SOUR{channel}:FREQ {frequency} MHZ")
+
+    @validate_channel
+    def set_mode(self, channel, mode):
+        if mode == AbstractSignalGenerator.Mode.SINUSOID:
+            self.instrument.write(f"SOUR{channel}:FUNC SIN")
+        elif mode == AbstractSignalGenerator.Mode.USER:
+            self.instrument.write(f"SOUR{channel}:FUNC ARBB")
+        else:
+            raise ValueError(f"Unsupported mode: {mode}.")
+
+    @validate_channel
+    def set_trigger_frequency_Hz(self, channel, frequency):
+        self.instrument.write(f"ARM:FREQ{channel} {frequency} HZ")
+
+    @validate_channel
+    def set_trigger_source(self, channel, source):
+        if source == AbstractSignalGenerator.TriggerSource.CONTINUOUS:
+            self.instrument.write(f"SOUR{channel}:BURST:STATE ON")
+            self.instrument.write(f"SOUR{channel}:BURST:NCYCLES INF")
+        elif source == AbstractSignalGenerator.TriggerSource.EXTERNAL:
+            self.instrument.write(f"ARM:SOUR{channel} EXT")
+        elif source == AbstractSignalGenerator.TriggerSource.INTERNAL:
+            self.instrument.write(f"ARM:SOUR{channel} INT2")
+        else:
+            raise ValueError(f"Unsupported trigger source: {source}.")
+    
+    @validate_channel
+    def set_offset(self, channel, offset):
+        self.instrument.write(f"SOUR{channel}:VOLT:OFFS {offset} mV")
+
+    def set_burst_mode_delay(self, delay):
+        self.instrument.write("SOUR1:BURS:MODE TRIG")
+        self.instrument.write("SOUR2:BURS:MODE TRIG")
+        self.instrument.write("TRIG:TIM 1")
+        self.instrument.write("SOUR1:BURS:NCYC 1")
+        self.instrument.write("SOUR2:BURS:NCYC 1")
+        self.instrument.write(f"SOUR2:BURS:TDEL {delay} ms")
+        self.run_instrument()
+
+
+    @validate_channel
+    def set_waveform(self, channel, waveform, amplitude):
+
+        with open(waveform, "r") as f:
+            dic = json.load(f)
+        waveform = dic["wf"]
+        freq = dic["freq_wf"]
+
+        if np.min(waveform) < 0:
+            waveform = np.array(waveform) + np.abs(np.min(waveform))
+
+        range_bit = 2**14
+        range_input = np.abs(np.max(waveform)) - np.max(np.min(waveform))
+        range_volt = amplitude
+        
+        scale_input_to_bit = range_bit / range_input 
+        waveform_bit = (np.array(waveform) * scale_input_to_bit)
+
+        scale_bit_to_volt = range_volt / range_bit
+        waveform_volt = (np.array(waveform_bit) * scale_bit_to_volt)
+
+        Vpp = np.max(waveform_volt) - np.min(waveform_volt)    
+        Vmean = np.mean(waveform_volt[0:20])
+        offset_volt = (Vpp/2) - Vmean
+        data = []
+        for i in waveform_bit:
+            data.append(int(i))
+
+        cmd = f"TRACE{channel}:DATA #{len(str(len(data)*2))}{len(data)*2}" #two bytes per sample
+        b = bytearray()
+        b.extend(map(ord, cmd))
+        for sample in data:
+            b.extend(struct.pack(">H", sample))
+
+        self.instrument.write_raw(b)
+        self.set_amplitude_mVpp(channel, amplitude)
+        self.set_offset(channel, offset_volt)
+        self.set_frequency_MHz(channel, freq)
+        self.run_instrument()
+
---
+++ tests/AUXTriggerResponse.py
@@ -0,0 +1,112 @@
+import argparse
+import json
+import radiant_test
+import stationrc.common
+import stationrc.remote_control
+import numpy as np
+import uproot
+
+parser = argparse.ArgumentParser()
+
+class AUXTriggerResponse(radiant_test.RADIANTTest):
+    def __init__(self):
+        super(AUXTriggerResponse, self).__init__()
+        self.awg = radiant_test.AWG4022(self.conf['args']['ip_address'])
+        
+    def initialize_signal_gen(self, channel, waveform, amplitude):
+        self.awg.output_off(channel)
+        self.awg.set_mode(channel, radiant_test.AWG4022.Mode.USER)
+        self.awg.set_waveform(channel, waveform, amplitude)
+        self.awg.set_trigger_source(channel, radiant_test.Keysight81160A.TriggerSource.CONTINUOUS)
+        self.awg.output_on(channel)
+
+    def initialize_config(self, channel_test, channel_clock, threshold, run_length):
+        run = stationrc.remote_control.Run(self.device)
+        for ch in range(24):
+            run.run_conf.radiant_threshold_initial(ch, threshold)
+
+
+        run.run_conf.radiant_load_thresholds_from_file(False)
+        run.run_conf.radiant_servo_enable(False)
+
+        run.run_conf.radiant_trigger_rf0_mask(channel_clock) # trigger for clock
+        run.run_conf.radiant_trigger_rf0_num_coincidences(1)
+        run.run_conf.radiant_trigger_rf0_enable(True)
+
+        run.run_conf.radiant_trigger_rf1_mask(channel_test) # trigger for test channel
+        run.run_conf.radiant_trigger_rf1_num_coincidences(1)
+        run.run_conf.radiant_trigger_rf1_enable(True)
+
+        run.run_conf.radiant_trigger_soft_enable(False)  # no forced trigger
+        run.run_conf.flower_device_required(False)
+        run.run_conf.flower_trigger_enable(False)
+        run.run_conf.run_length(run_length)
+        run.run_conf.comment("AUX Trigger Response Test")
+        self.data_dir = run.start(delete_src=True, rootify=True)
+
+    def calc_trigger(self, root_file, ch_test, ch_clock):
+
+        n_total = 60
+        f = uproot.open(root_file)
+        data = f["combined"]
+
+        waveforms = np.array(data['waveforms/radiant_data[24][2048]'])  #events, channels, samples
+        thresholds = np.array(data['daqstatus/radiant_thresholds[24]'])
+        thresh = np.round(thresholds[:, ch_test]*2.5/(2**24-1),2)[10]
+
+        mask_radiant_trigger = data['header/trigger_info/trigger_info.radiant_trigger'].array() == True
+        mask_rf0 = data['header/trigger_info/trigger_info.which_radiant_trigger'].array() == 0
+        mask_rf1 = data['header/trigger_info/trigger_info.which_radiant_trigger'].array() == 1
+        rf0_true = mask_radiant_trigger & mask_rf0
+        rf1_true = mask_radiant_trigger & mask_rf1
+        
+
+
+        index_max_amp_clock = np.argmax(np.abs(waveforms[:, ch_clock, :]), axis=1)
+        pulse_clock_correct = (1450 < index_max_amp_clock) & (index_max_amp_clock < 1750)
+        index_max_amp_test = np.argmax(np.abs(waveforms[:, ch_test, :]), axis=1)
+        pulse_test_correct = (1450 < index_max_amp_test) & (index_max_amp_test < 1750)
+        clock_amp = (np.max(np.abs(waveforms[:, ch_clock, :]), axis=1)) > 200
+
+        rf0_pulse = rf0_true & pulse_clock_correct & clock_amp
+        rf1_pulse = rf1_true & pulse_test_correct & clock_amp
+
+        print(waveforms[rf0_pulse,ch_test,:].shape)
+        print(waveforms[rf1_pulse,ch_test,:].shape)
+
+        print(waveforms[rf0_pulse,ch_test,:])
+
+        trig_eff = waveforms[rf1_pulse,ch_test,:].shape[0] / waveforms[rf1_pulse,ch_clock,:].shape[0]
+        max_amp = (np.max(np.abs(waveforms[rf1_pulse,ch_test,:]), axis=1))
+        snr = max_amp / (np.sqrt(np.mean(waveforms[rf1_pulse,ch_test:1000]**2, axis=1)))
+
+        self.dic = {}
+        self.dic['trig_eff'] = trig_eff
+        self.dic['snr'] = snr
+        self.dic['threshold'] = thresh
+        self.dic['root_dir'] = root_file
+    
+    def eval_results(self, data):
+        passed = False
+
+        threshold = data['threshold']
+        if (
+            data['trig_eff'] >= self.conf["expected_values"][threshold]["num_min"]
+            and data['trig_eff'] < self.conf["expected_values"][threshold]["num_max"]
+        ):
+            passed = True
+        
+        self.add_measurement("AUXTriggerResponse", data, passed)
+
+    def run(self):
+        super(AUXTriggerResponse, self).run()
+        self.initialize_signal_gen(self.conf['args']['ch_sg'], self.conf['args']['waveform'], self.conf['args']['amplitude']) #set test channel
+        self.initialize_signal_gen(self.conf['args']['ch_sg_clock'], self.conf['args']['waveform'], self.conf['args']['clock_amplitude']) #set clock channel
+        self.awg.set_burst_mode_delay(100)
+        self.awg.run_instrument()
+        self.initialize_config(self.conf['args']['ch_radiant'], self.conf['args']['ch_radiant_clock'], self.conf['args']['threshold'], self.conf['args']['run_length'])
+        self.calc_trigger(self.data_dir/"combined.root", self.conf['args']['ch_radiant'], self.conf['args']['ch_radiant_clock'])
+        self.eval_results(self.dic)
+
+if __name__ == "__main__":
+    radiant_test.run(AUXTriggerResponse)
