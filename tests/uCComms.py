bdf215002cd0065b7871f8dacd230133fb3e4b72
Felix Schlüter <fschlueter@icecube.wisc.edu>
2023-11-09 16:16:16 +0100

    Allow to specify a list of allowed board manager versions and data

--- tests/uCComms.py
+++ tests/uCComms.py
@@ -1,11 +1,27 @@
 import radiant_test
 import stationrc.radiant
-
+import numpy as np
 
 class uCComms(radiant_test.RADIANTTest):
     def __init__(self):
         super(uCComms, self).__init__()
-
+        
+    def _add_measurements(self, name, measurement, reference):
+        if isinstance(reference, list):
+            self.add_measurement(
+                name,
+                measurement,
+                np.any([measurement == ele for ele in reference]),
+            )
+        elif isinstance(reference, str):
+            self.add_measurement(
+                name,
+                measurement,
+                measurement == reference,
+            )
+        else:
+            raise ValueError(f"Expected value for \"{name}\" has wrong type.")
+        
     def run(self):
         super(uCComms, self).run()
 
@@ -21,18 +37,12 @@ class uCComms(radiant_test.RADIANTTest):
         board_manager_date_version = stationrc.radiant.DateVersion(
             self.device.radiant_low_level_interface.read_register("BM_DATEVERSION")
         ).toDict()
-        self.add_measurement(
-            "board_manager_date",
-            board_manager_date_version["date"],
-            board_manager_date_version["date"]
-            == self.conf["expected_values"]["board_manager_date"],
-        )
-        self.add_measurement(
-            "board_manager_version",
-            board_manager_date_version["version"],
-            board_manager_date_version["version"]
-            == self.conf["expected_values"]["board_manager_version"],
-        )
+        
+        self._add_measurements("board_manager_date", board_manager_date_version["date"],
+                               self.conf["expected_values"]["board_manager_date"])
+        
+        self._add_measurements("board_manager_version", board_manager_date_version["version"],
+                               self.conf["expected_values"]["board_manager_version"])
 
 
 if __name__ == "__main__":
