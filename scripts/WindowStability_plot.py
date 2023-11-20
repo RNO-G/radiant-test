7e28d1f2b28925a92e142feab6d0dbfea05c1f57
Felix Schlüter <fschlueter@icecube.wisc.edu>
2023-11-20 14:32:38 +0100

    refactor code scripts/WindowStability_plot.py

--- scripts/WindowStability_plot.py
+++ scripts/WindowStability_plot.py
@@ -35,6 +35,7 @@ def print_results(data):
     
     measurements = dict(sorted(data["run"]["measurements"].items(), key=lambda x: int(x[0])))
     config = data["config"]
+    add_color = lambda b: colorama.Fore.GREEN if b else colorama.Fore.RED
         
     result_str = f'{"CH":<5} | {"variation < {}".format(config["expected_values"]["variation_tolerance"]):^20} | ' + \
         f'{"min_power > {}".format(config["expected_values"]["min_power"]):^20} | ' + \
@@ -44,41 +45,19 @@ def print_results(data):
 
     for ch, ele in measurements.items():        
         result = ele["result"]
-                
-        # if args_channel is not None and args_channel != idx:  # skip 
-        #     continue
         mean_variation, min_power, max_power, _, _ = calculate_variation(ele["measured_value"])
 
         # Create string for terminal output
-        
-        if result == "PASS":
-            result_str += colorama.Fore.GREEN 
-        else:
-            result_str += colorama.Fore.RED
+        result_str += add_color(result == "PASS") + f"{ch:<5}" + colorama.Style.RESET_ALL + " | "
             
-        result_str += f"{ch:<5}" + colorama.Style.RESET_ALL + " | "
-        
-        if mean_variation < config["expected_values"]["variation_tolerance"]:
-            result_str += colorama.Fore.GREEN 
-        else:
-            result_str += colorama.Fore.RED 
+        result_str += add_color(mean_variation < config["expected_values"]["variation_tolerance"]) + \
+            f"{mean_variation:^20.2f}" + colorama.Style.RESET_ALL + " | "
             
-        result_str += f"{mean_variation:^20.2f}" + colorama.Style.RESET_ALL + " | "
-        
-        if min_power > config["expected_values"]["min_power"]:
-            result_str += colorama.Fore.GREEN 
-        else:
-            result_str += colorama.Fore.RED 
+        result_str += add_color(min_power > config["expected_values"]["min_power"]) + \
+            f"{min_power:^20.2f}" + colorama.Style.RESET_ALL + " | "
             
-        result_str += f"{min_power:^20.2f}" + colorama.Style.RESET_ALL + " | "
-
-        if max_power < config["expected_values"]["max_power"]:
-            result_str += colorama.Fore.GREEN 
-        else:
-            result_str += colorama.Fore.RED 
-            
-        result_str += f"{max_power:^20.2f}" + colorama.Style.RESET_ALL
-        result_str += "\n"
+        result_str += add_color(max_power < config["expected_values"]["max_power"]) + \
+            f"{max_power:^20.2f}" + colorama.Style.RESET_ALL + "\n"
         
     print(result_str)
         
