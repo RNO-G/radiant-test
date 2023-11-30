c0a8965c67534949e961a3a9e95c7f153cfab6b1
Felix Schlüter <fschlueter@icecube.wisc.edu>
2023-11-30 17:12:41 +0100

    Also add interface to single tests. add script run_test.py

--- radiant_test/run.py
+++ radiant_test/run.py
@@ -2,8 +2,8 @@ from .RADIANTTest import RADIANTTest
 from .radiant_helper import get_radiant
 
 
-def run(test_class):
-    test = test_class()
+def run(test_class, kwargs={}):
+    test = test_class(**kwargs)
     if issubclass(test_class, RADIANTTest):
         test.device = get_radiant()
     test.initialize()
