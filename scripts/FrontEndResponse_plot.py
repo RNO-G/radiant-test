3fc04c7b2019e48f684669188c0fb0f52e293cc4
Radio Lab <rnog@ecap-l023.physik.uni-erlangen.de>
2023-12-05 19:35:23 +0100

    change FrontEndResponse so that it is based on SignalGen2LAB4D measurements

---
+++ scripts/FrontEndResponse_plot.py
@@ -0,0 +1,131 @@
+import numpy as np
+import json
+import matplotlib.pyplot as plt
+import os
+import argparse
+import glob
+import colorama
+from matplotlib.cm import viridis
+
+def get_rows_cols(n=24):
+    if n <= 9:
+        nrows = int(np.ceil(n / 3))
+        ncols = n if n < 3 else 3
+    else:
+        nrows = int(np.ceil(n / 6))
+        ncols = 6
+    return nrows, ncols
+
+def get_color(passed):
+    if passed:
+        return colorama.Fore.GREEN 
+    else:
+        return colorama.Fore.RED
+    
+def get_axis_color(ax, passed):
+    if passed == 'PASS':
+        color = '#6da34d'
+    elif passed == 'FAIL':
+        color = '#B50A1B'
+    ax.spines['bottom'].set_color(color)  # x-axis
+    ax.spines['top'].set_color(color)
+    ax.spines['left'].set_color(color)   # y-axis
+    ax.spines['right'].set_color(color)
+
+def get_results_str(data, ch, with_color=False):
+    data_ch = data["run"]["measurements"][str(ch)]["measured_value"]["xcorr_mean"]
+    result = data["run"]["measurements"][str(ch)]["result"]
+    color_end = colorama.Style.RESET_ALL
+    str_xcorr = get_color(result) + f'xcorr mean: {data_ch:.2f}{color_end}'
+    for amp_str in get_key_amps(data, ch):
+        xcorr = data["run"]["measurements"][str(ch)]["measured_value"][amp_str]["xcorr"]
+        res_amp = data["run"]["measurements"][str(ch)]["measured_value"][amp_str]["res_xcorr"]
+        str_xcorr += f'{get_color(res_amp)} | {amp_str} mVpp: {xcorr:.2f} {color_end}'
+    out = f"{get_color(result == 'PASS')} {result} {color_end} | {str_xcorr}"    
+    return out
+
+def get_channels(data):
+    return sorted([int(ch) for ch in data["run"]["measurements"].keys()])
+
+def get_key_amps(data, ch):
+    amps = []
+    for amp in data["run"]["measurements"][str(ch)]["measured_value"].keys():
+        if amp.isnumeric():
+            amps.append(amp)
+    return amps        
+
+def get_measured_values(data):
+    measured_val_dict = {'channel': [], 'result': [], 'xcorr': []}
+    for ch in get_channels(data):
+        fit_params = data['run']['measurements'][f"{ch}"]['measured_value']['fit_parameter']
+        measured_val_dict['channel'].append(ch)
+        measured_val_dict['result'].append(data['run']['measurements'][f"{ch}"]['result'])
+        measured_val_dict['xcorr'].append(fit_params['xcorr'])
+    return measured_val_dict
+
+def print_results(data, channel=None):
+    if channel is None:
+        for ch in get_channels(data):
+            print(f"ch. {ch:2d} - {get_results_str(data, ch, with_color=True)}")                
+    else:
+        print(f"ch. {channel:2d} - {get_results_str(data, channel, with_color=True)}")
+
+def plot_all(data, args_input="", args_channel=None, args_web=False):
+    nrows, ncols = get_rows_cols()
+    fig = plt.figure(figsize=(12,6))
+    axs = fig.subplots(nrows=nrows, ncols=ncols)
+    for ch in get_channels(data):
+        if nrows == 1:
+            ax = axs[ch % ncols]
+        else:
+            ax = axs[ch // ncols][ch % ncols]
+        plot_channel(fig, ax, data, ch)
+    fig.tight_layout()
+    if args_web:
+        return fig
+
+def plot_single(data, ch):
+    fig = plt.figure()
+    ax = fig.subplots()
+    plot_channel(fig, ax, data, ch)
+
+def plot_channel(fig, ax, data, ch):
+    vals = data['run']['measurements'][f"{ch}"]['measured_value']
+    print(vals.keys())
+    truth_wf = np.array(vals['truth_waveform'])
+
+    key_amps = get_key_amps(data, ch)
+    colors = viridis(np.linspace(0, 1, len(key_amps)))
+    for i, amp_str in enumerate(key_amps):
+        xcorr = vals[amp_str]['xcorr']
+        measured_wf = np.array(vals[amp_str]['measured_waveform'])
+        i_max = np.argmax(measured_wf)
+        wf = (measured_wf[i_max-200:i_max+300]/np.max(measured_wf))
+        ax.plot(wf, alpha=0.5, color=colors[i], label=f'{amp_str} mVpp')
+        ax.legend(loc='lower right')
+    ax.plot(truth_wf, color='red', ls=':')
+    res = data['run']['measurements'][f"{ch}"]['result']
+    ax.set_title(f'channel: {ch}')
+    fig.text(0.5, 0.01, 'samples', ha='center', va='center')
+    fig.text(0.01, 0.5, 'normalized amplitude [a.u.]', ha='center', va='center', rotation='vertical')
+    get_axis_color(ax, res)
+
+if __name__ == "__main__":
+    import argparse
+    import json
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input", help="input JSON file")
+    parser.add_argument("-c", "--channel", type=int, help="only plot single channel")
+    parser.add_argument("-w", "--web", action="store_true", help="Return figures to be displayed in web")
+    args = parser.parse_args()
+
+    with open(args.input, "r") as f:
+        data = json.load(f)
+    if args.channel == None:
+        plot_all(data, args_input=args.input, args_channel=args.channel, args_web=args.web)
+        print_results(data)
+    else:
+        plot_single(data, args.channel)
+        print_results(data, args.channel)
+    plt.show()
--- tests/FrontEndResponse.py
+++ tests/FrontEndResponse.py
@@ -1,70 +1,145 @@
 from __future__ import absolute_import, division, print_function
 import json
 import radiant_test
-import stationrc.common
-import stationrc.remote_control
 import numpy as np
 import uproot
-import scipy
 import numpy as np
-import fractions
-import decimal
-import scipy.signal
+from numpy import linalg as LA
+import glob
+import os
+import radiant_test.radiant_helper as rh
+import logging
+
 
 class FrontEndResponse(radiant_test.RADIANTChannelTest):
     def __init__(self):
         super(FrontEndResponse, self).__init__()
-        if self.site_conf['test_site'] == 'ecap':
-            self.awg = radiant_test.AWG4022(self.site_conf['signal_gen_ip_address'])
-        elif self.site_conf['test_site'] == 'desy':
-            self.awg = radiant_test.Keysight81160A(self.site_conf['signal_gen_ip_address'])
-        else:
-            raise ValueError("Invalid test_site, use desy or ecap")
-        
-    def initialize_config(self, channel, threshold, run_length):
-        run = stationrc.remote_control.Run(self.device)
-        for ch in range(24):
-            run.run_conf.radiant_threshold_initial(ch, threshold)
 
-        run.run_conf.radiant_trigger_rf0_enable(True)
-        run.run_conf.radiant_trigger_rf0_mask([channel])
-        run.run_conf.radiant_trigger_rf0_num_coincidences(1)
-        run.run_conf.radiant_trigger_rf1_enable(False)
-        run.run_conf.radiant_trigger_soft_enable(False)  # no forced trigger
-        run.run_conf.flower_device_required(False)
-        run.run_conf.flower_trigger_enable(False)
-        run.run_conf.run_length(run_length)
-        run.run_conf.comment("FrontEndResponse Test")
-        self.data_dir = run.start(delete_src=True, rootify=True)
+    def get_root_files(self, search_dir, channel):
+        #radiant_name = rh.uid_to_name(self.result_dict["dut_uid"])
+        radiant_name = 'ULB-008'
+        print(os.path.join(search_dir, f"{radiant_name}_SignalGen2LAB4D_*.json"))
+        files = glob.glob(os.path.join(search_dir, f"{radiant_name}_SignalGen2LAB4D_*.json"))
+        files_sorted = sorted(files)
+        last_test = files_sorted[-1]    
+        with open(last_test, "r") as f:
+            data = json.load(f)
+        vals = data['run']['measurements'][str(channel)]['measured_value']
+        logging.warning(f"evaluate FrontEndResponse for channel {channel} based on {last_test}")
+        root_dirs = []
+        amps = []
+        for key in vals['raw_data']:
+            root_dir = vals['raw_data'][key]['run']
+            amp = vals['raw_data'][key]['amp']
+            root_dirs.append(root_dir)
+            amps.append(amp)
+        return root_dirs, amps
+    
+    def get_truth_waveform(self, template):
+        with open(template, "r") as f:
+            data = json.load(f)
+        return np.array(data['waveform'])
 
-
-    def calc_response(self, root_file):
-        template_time, template_wf = self.get_template_innput(self.conf['args']['waveform'])
+    def get_measured_waveforms(self, root_file, ch):
         f = uproot.open(root_file)
         data = f["combined"]
         waveforms = np.array(data['waveforms/radiant_data[24][2048]'])  #events, channels, samples
-        #time = np.arange(0,len(waveforms),1)/3.2
-        cc = scipy.signal.correlate(waveforms, self.conf['args']['waveform'])
+        return waveforms[:,ch,:]
 
-    def eval_results(self, data):
+    def calc_xcorr(self, dataTrace, templateTrace, window_size=200*1e-9, sampling_rate=3.2*1e9, return_time_difference=False):
+        # preparing the traces
+        dataTrace = np.float32(dataTrace)
+        templateTrace = np.float32(templateTrace)
+
+        # create the template window
+        window_steps = window_size * (sampling_rate)
+
+        max_amp = max(abs(templateTrace))
+        max_amp_i = np.where(abs(templateTrace) == max_amp)[0][0]
+        lower_bound = int(max_amp_i - window_steps / 3)
+        upper_bound = int(max_amp_i + 2 * window_steps / 3)
+        templateTrace = templateTrace[lower_bound:upper_bound]
+
+        # zero padding on the data trace
+        dataTrace = np.append(np.zeros(len(templateTrace) - 1), dataTrace)
+        dataTrace = np.append(dataTrace, np.zeros(len(templateTrace) - 1))
+
+        # only calculate the correlation of the part of the trace where at least 10% of the maximum is visible (fastens the calculation)
+        max_amp_data = max(abs(dataTrace))
+        help_val = np.where(abs(dataTrace) >= 0.1 * max_amp_data)[0]
+        lower_bound_data = help_val[0] - (len(templateTrace) - 1)
+        upper_bound_data = help_val[len(help_val) - 1] + (len(templateTrace) - 1)
+        dataTrace = dataTrace[lower_bound_data:upper_bound_data]
+
+        # run the correlation using matrix multiplication
+        dataMatrix = np.lib.stride_tricks.sliding_window_view(dataTrace, len(templateTrace))
+        corr_numerator = dataMatrix.dot(templateTrace)
+        norm_dataMatrix = LA.norm(dataMatrix, axis=1)
+        norm_templateTrace = LA.norm(templateTrace)
+        corr_denominator = norm_dataMatrix * norm_templateTrace
+        correlation = corr_numerator / corr_denominator
+
+        max_correlation = max(abs(correlation))
+        max_corr_i = np.where(abs(np.asarray(correlation)) == max_correlation)[0][0]
+
+        if return_time_difference:
+            # calculate the time difference between the beginning of the template and data trace for the largest correlation value
+            # time difference is given in ns
+            time_diff = (max_corr_i + (lower_bound_data - len(templateTrace))) / sampling_rate   
+        if return_time_difference:
+            return max_correlation, time_diff
+        else:
+            return max_correlation
+    
+    def eval_results(self, data, channel):
         passed = False
-        template = self.conf['args']['waveform']
-        if (
-            data['crosscorr'] >= self.conf["expected_values"][template]["num_min"]
-            and data['crosscorr'] < self.conf["expected_values"][template]["num_max"]
-        ):
+        all_passed = []
+        for key in data:
+            if key in ['truth_waveform', 'xcorr_mean']:
+                continue
+            else:
+                val = data[key]['xcorr']
+                if (
+                    val >= self.conf["expected_values"]["xcorr_min"]
+                    and val < self.conf["expected_values"]["xcorr_max"]
+                ):
+                    passed_single = True
+                else:
+                    passed_single = False
+
+                all_passed.append(passed_single)
+                data[key]['res_xcorr'] = passed_single
+        if False in all_passed:
+            passed = False
+        else:        
             passed = True
-        
-        self.add_measurement("FrontEndResponse", data, passed)
+        self.add_measurement(f"{channel}", data, passed)
 
     def run(self):
         super(FrontEndResponse, self).run()
-        self.awg.output_off(self.conf['args']['ch_sg'])
-        self.awg.set_waveform(self.conf['args']['ch_sg'], self.conf['args']['waveform'])
-        self.awg.set_amplitude_mVpp(self.conf['args']['ch_sg'], 800)
-        self.awg.output_on(self.conf['args']['ch_sg'])
-        self.initialize_config(channel=8, threshold=self.conf['args']['threshold'], run_length=20)
-        self.eval_results(self.dic)
+        for ch in self.conf['args']['channels']:
+            data = {}
+            wf_truth = self.get_truth_waveform(self.conf['args']['template'])
+            data['truth_waveform'] = wf_truth.tolist()
+            root_files, amps = self.get_root_files(self.conf['args']['search_dir'], ch)
+            ccs = []
+            for (root_file, amp) in zip(root_files, amps):
+                key = f'{amp:.0f}'
+                data[key] = {}
+                data[key]['root_files'] = root_file
+                wfs_measured = self.get_measured_waveforms(root_file, ch)
+                ccs = []
+                for i in range(len(wfs_measured[1:-1,0])): #loop over events
+                    if i == 0:
+                        data[key]['measured_waveform'] = wfs_measured[i,:].tolist()
+                    wf_measured = wfs_measured[i,:]
+                    cc = self.calc_xcorr(wf_measured, wf_truth)
+                    ccs.append(cc)
+                cc = np.mean(ccs)
+                data[key]['xcorr'] = cc
+                ccs.append(cc)
+            data['xcorr_mean'] = np.mean(ccs)
+            self.eval_results(data, ch)
 
 if __name__ == "__main__":
     radiant_test.run(FrontEndResponse)
