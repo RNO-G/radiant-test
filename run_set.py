2f9cfc80a1289077f31543338d3c257649a729ff
Felix Schlüter <felix.schluter@icecube.wisc.edu>
2023-12-13 16:19:38 +0100

    Add --debug flag to exe scripts

--- run_set.py
+++ run_set.py
@@ -1,6 +1,5 @@
 import argparse
 import logging
-logging.basicConfig(level=logging.INFO)
 
 import radiant_test
 
@@ -8,8 +7,13 @@ import radiant_test
 parser = argparse.ArgumentParser()
 parser.add_argument("test_set", type=str, help="test set to execute")
 parser.add_argument("--comment", type=str, help="add a comment to the result dict of every test")
+parser.add_argument("--debug", action="store_true", help="Set logger setting to DEBUG")
 args = parser.parse_args()
 
+if args.debug:
+    logging.basicConfig(level=logging.DEBUG)
+else:
+    logging.basicConfig(level=logging.INFO)
 
 test_set = radiant_test.TestSet(args.test_set, device=radiant_test.get_radiant(), comment=args.comment)
 test_set.run()
--- run_test.py
+++ run_test.py
@@ -1,6 +1,6 @@
 import argparse
 import logging
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.DEBUG)
 
 import radiant_test
 
@@ -10,7 +10,13 @@ tests = [t for t in module.__dir__() if not t.startswith("__")]
 parser = argparse.ArgumentParser()
 parser.add_argument("test", type=str, choices=tests, help="Test to execute")
 parser.add_argument("--comment", type=str, default=None, help="add a comment to the result dict of every test")
+parser.add_argument("--debug", action="store_true", help="Set logger setting to DEBUG")
 args = parser.parse_args()
+
+if args.debug:
+    logging.basicConfig(level=logging.DEBUG)
+else:
+    logging.basicConfig(level=logging.INFO)
 
 test = getattr(module, args.test)
 radiant_test.run(test, {"comment": args.comment})
