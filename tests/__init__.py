1f2fc5d516d54fd26179e21cad24dd0767431f7c
Felix Schlüter <felix.schluter@icecube.wisc.edu>
2024-01-05 12:37:03 +0100

    Add try-except block for every test import. This reduces necessary dependencies

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,14 +1,69 @@
-from .DummyTest import DummyTest
-from .FailingTest import FailingTest
-from .FrontEndNoise import FrontEndNoise
-from .FPGAComms import FPGAComms
-from .SigGenSine import SigGenSine
-from .HarmonicDistortion import HarmonicDistortion
-from .LAB4DGlitch import LAB4DGlitch
-from .LAB4DTune import LAB4DTune
-from .RADIANTPower import RADIANTPower
-from .SystemPower import SystemPower
-from .uCComms import uCComms
-from .WindowStability import WindowStability
-from .BiasScan import BiasScan
-from .SignalGen2LAB4D import SignalGen2LAB4D
+try:
+    from .DummyTest import DummyTest
+except ImportError as e:
+    print(f"DummyTest not available. {e}")
+
+try:
+    from .FailingTest import FailingTest
+except ImportError as e:
+    print(f"FailingTest not available. {e}")
+
+try:
+    from .FrontEndNoise import FrontEndNoise
+except ImportError as e:
+    print(f"FrontEndNoise not available. {e}")
+
+try:
+    from .FPGAComms import FPGAComms
+except ImportError as e:
+    print(f"FPGAComms not available. {e}")
+
+try:
+    from .SigGenSine import SigGenSine
+except ImportError as e:
+    print(f"SigGenSine not available. {e}")
+
+try:
+    from .HarmonicDistortion import HarmonicDistortion
+except ImportError as e:
+    print(f"HarmonicDistortion not available. {e}")
+
+try:
+    from .LAB4DGlitch import LAB4DGlitch
+except ImportError as e:
+    print(f"LAB4DGlitch not available. {e}")
+
+try:
+    from .LAB4DTune import LAB4DTune
+except ImportError as e:
+    print(f"LAB4DTune not available. {e}")
+
+try:
+    from .RADIANTPower import RADIANTPower
+except ImportError as e:
+    print(f"RADIANTPower not available. {e}")
+
+try:
+    from .SystemPower import SystemPower
+except ImportError as e:
+    print(f"SystemPower not available. {e}")
+
+try:
+    from .uCComms import uCComms
+except ImportError as e:
+    print(f"uCComms not available. {e}")
+
+try:
+    from .WindowStability import WindowStability
+except ImportError as e:
+    print(f"WindowStability not available. {e}")
+
+try:
+    from .BiasScan import BiasScan
+except ImportError as e:
+    print(f"BiasScan not available. {e}")
+
+try:
+    from .SignalGen2LAB4D import SignalGen2LAB4D
+except ImportError as e:
+    print(f"SignalGen2LAB4D not available. {e}")
