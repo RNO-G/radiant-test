8130e2f718aff505451d85a416f7a1b84391d58c
Felix Schlüter <felix.schluter@icecube.wisc.edu>
2024-01-22 22:08:43 +0100

    Allow to run several tests at once

--- run_test.py
+++ run_test.py
@@ -7,7 +7,7 @@ module = __import__("tests")
 tests = [t for t in module.__dir__() if not t.startswith("__")]
 
 parser = argparse.ArgumentParser()
-parser.add_argument("test", type=str, choices=tests, help="Test to execute")
+parser.add_argument("tests", type=str, nargs="+", choices=tests, help="Test to execute")
 parser.add_argument("--comment", type=str, default=None, help="add a comment to the result dict of every test")
 parser.add_argument("--debug", action="store_true", help="Set logger setting to DEBUG")
 parser.add_argument("--plot", action="store_true", help="Run the plotting script (if available)")
@@ -18,14 +18,15 @@ if args.debug:
 else:
     logging.basicConfig(level=logging.INFO)
 
-test_class = getattr(module, args.test)
-test = radiant_test.run(test_class, {"comment": args.comment})
-if args.plot:
-    scripts = __import__("scripts")
-    try:
-        import os
-        script = getattr(scripts, args.test + "_plot")
-        print(f"Run python {script.__file__} {test.fname}")
-        os.system(f"python {script.__file__} {test.fname}")
-    except AttributeError:
-        print(f"Not plotting script for {args.test} found!")
+for test in args.tests:
+    test_class = getattr(module, test)
+    test_obj = radiant_test.run(test_class, {"comment": args.comment})
+    if args.plot:
+        scripts = __import__("scripts")
+        try:
+            import os
+            script = getattr(scripts, test + "_plot")
+            print(f"Run python {script.__file__} {test_obj.fname}")
+            os.system(f"python {script.__file__} {test_obj.fname}")
+        except AttributeError:
+            print(f"Not plotting script for {test} found!")
