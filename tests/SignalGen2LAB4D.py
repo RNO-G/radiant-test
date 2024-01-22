7b091dc972c6fe10f240c465feb220e60ec67767
Felix Schlüter <felix.schluter@icecube.wisc.edu>
2024-01-22 19:25:20 +0100

    Significant refactoring of SignalGen2LAB4D.py. This mostly cleans up code and reorganizes it slightly. However, it implements the option to read the binary files rather than rootyfing them. This is not yet implemented

--- tests/SignalGen2LAB4D.py
+++ tests/SignalGen2LAB4D.py
@@ -13,6 +13,24 @@ import pathlib
 import radiant_test.radiant_helper as rh
 import time
 import sys
+from collections import defaultdict
+
+
+def confirm_or_abort():
+    confirmation_signal = None
+    while confirmation_signal != "":
+        try:
+            confirmation_signal = input("Press Enter to confirm: ")
+        except KeyboardInterrupt:
+            print("Keyboard interrupt. Exiting...")
+            sys.exit(1)
+
+def check_param(param_value, param_min, param_max):
+    if param_value is None:
+        return False
+    elif not param_min < param_value < param_max:
+        return False
+    return True
 
 def make_serializable(obj):
     if isinstance(obj, dict):
@@ -41,7 +59,7 @@ def calc_sliding_vpp(data, window_size=30, start_index=1400, end_index=1900):
     return vpps, indices
 
 
-class SignalGen2LAB4D(radiant_test.RADIANTTest):
+class SignalGen2LAB4D(radiant_test.RADIANTChannelTest):
     def __init__(self):
         super(SignalGen2LAB4D, self).__init__()
         self.awg = radiant_test.Keysight81160A(
@@ -70,33 +88,30 @@ class SignalGen2LAB4D(radiant_test.RADIANTTest):
                 self.arduino.route_signal_to_channel(radiant_ch_clock)
         else:
             raise ValueError("Invalid channel number")
+
         return sg_ch, sg_ch_clock, radiant_ch_clock
-    
-    def start_run(self, station, run_conf, delete_src=False, rootify=False):
+
+    def start_run(self, run_conf, delete_src=False):
+        station = self.device
         station.set_run_conf(run_conf)
         res = station.daq_run_start()
 
         # start pulsing
         time.sleep(10)
-        self.awg.send_n_software_triggers(n_trigger=self.conf["args"]["number_of_events"], trigger_rate=self.conf["args"]["sg_trigger_rate"])
+        self.awg.send_n_software_triggers(
+            n_trigger=self.conf["args"]["number_of_events"], trigger_rate=self.conf["args"]["sg_trigger_rate"])
 
         station.daq_run_wait()
         station.retrieve_data(res["data_dir"], delete_src=delete_src)
         data_dir = (
             pathlib.Path(station.station_conf["daq"]["data_directory"])
-            / pathlib.Path(res["data_dir"]).parts[-1]
-        )
-        if rootify:
-            stationrc.common.rootify(
-                data_dir,
-                station.station_conf["daq"]["mattak_directory"],
-            )
+            / pathlib.Path(res["data_dir"]).parts[-1])
+
         return data_dir
 
     def initialize_config(self, channel_trigger, threshold):
-        print('trigger set on channel', channel_trigger)
+        self.logger.info('trigger set on channel', channel_trigger)
         run = stationrc.remote_control.Run(self.device)
-        station = self.device
         for ch in range(24):
             run.run_conf.radiant_threshold_initial(ch, threshold)
 
@@ -114,17 +129,31 @@ class SignalGen2LAB4D(radiant_test.RADIANTTest):
         run_length = (self.conf["args"]["number_of_events"] * (1/self.conf["args"]["sg_trigger_rate"])) + 10
         run.run_conf.run_length(run_length)
         run.run_conf.comment("Signal Gen 2 LAB4D Amplitude Test")
-        self.data_dir = self.start_run(station, run.run_conf, delete_src=True, rootify=True)
 
-        print(f'start run stored at {self.data_dir}')
+        return run
 
     def get_vpp(self, root_file, ch, ch_clock, amp, tag, plot=False):
-        self.dic_run = {}
-        f = uproot.open(root_file)
-        data = f["combined"]
+        """ Calculate vpp/snr from measured data """
 
-        # events, channels, samples
-        wfs = np.array(data['waveforms/radiant_data[24][2048]'])
+        if self.conf['args']['rootify']:
+            stationrc.common.rootify(
+                self.data_dir, self.device.station_conf["daq"]["mattak_directory"])
+
+            root_file = self.data_dir / "combined.root"
+
+            f = uproot.open(root_file)
+            data = f["combined"]
+
+            # events, channels, samples
+            wfs = np.array(data['waveforms/radiant_data[24][2048]'])
+        else:
+            data = stationrc.common.dump_binary(
+                wfs_file=self.device.station_conf["daq"]["radiant-try-event_wfs_file"],
+                read_header=False, read_pedestal=False)
+            wfs = np.array([ele['radiant_waveforms'] for ele in data["data"]['WAVEFORM']])
+
+        self.dic_run = {}
+
         vpps = []
         vrms = []
         snrs = []
@@ -137,8 +166,8 @@ class SignalGen2LAB4D(radiant_test.RADIANTTest):
             vpps.append(float(max_vpp))
             vrm = np.std(wfs[i, ch, :800])
             vrms.append(vrm)
-            snrs.append(max_vpp/(2*vrm))
-            snr_pure_noise.append(max_vpp_noise/(2*vrm))
+            snrs.append(max_vpp / (2 * vrm))
+            snr_pure_noise.append(max_vpp_noise / (2 * vrm))
             if plot:
                 if i == 5:
                     fig, ax = plt.subplots()
@@ -155,7 +184,8 @@ class SignalGen2LAB4D(radiant_test.RADIANTTest):
                     ax.legend()
                     dir = (f'/home/rnog/radiant-test/scripts/plots/'
                         f'{rh.uid_to_name(self.result_dict["dut_uid"])}_'
-                        f'{self.name}_{datetime.datetime.fromtimestamp(self.result_dict["initialize"]["timestamp"]).strftime("%Y%m%dT%H%M%S")}')
+                        f'{self.name}_{datetime.datetime.fromtimestamp(
+                            self.result_dict["initialize"]["timestamp"]).strftime("%Y%m%dT%H%M%S")}')
 
                     if not os.path.exists(dir):
                         os.makedirs(dir)
@@ -170,172 +200,120 @@ class SignalGen2LAB4D(radiant_test.RADIANTTest):
         snr_err = np.std(snrs)
         snr_pure_noise_mean = np.mean(snr_pure_noise)
 
-        print(
-            f'getting Vpp for ch {ch} from clock trigger on ch {ch_clock}, Vpp is: '
-            f'{vpp_mean:.2f} +- {vpp_err:.2f}')
+        print(f'getting Vpp for ch {ch} from clock trigger on ch {ch_clock}, Vpp is: '
+              f'{vpp_mean:.2f} +- {vpp_err:.2f}')
 
         return vpp_mean, vpp_err, vrms_mean, snr_mean, snr_err, snr_pure_noise_mean, vpps, vrms, snrs, root_file
 
-    def get_vpp_on_the_fly(self, ch, ch_clock, thresh, n_events, amp, tag):
-        data = self.device.daq_record_data(num_events=n_events, trigger_channels=[
-                                           ch_clock], trigger_threshold=thresh, force_trigger=False)
-        waveforms = data["data"]["WAVEFORM"]
-        vpps = []
-        vrms = []
-        for i, event in enumerate(waveforms):
-            all_pps, indices = calc_sliding_vpp(event['radiant_waveforms'][ch])
-            max_vpp = np.max(all_pps)
-            sample_index = indices[np.argmax(all_pps)]
-            vpps.append(float(max_vpp))
-            vrm = np.std(event['radiant_waveforms'][ch][:50])
-            vrms.append(vrm)
-        vpp_mean = np.mean(vpps)
-        vpp_err = np.std(vpps)
-        vrms_mean = np.mean(vrms)
-        print(
-            f'getting Vpp for ch {ch} from clock trigger on ch {ch_clock}, Vpp is: {vpp_mean:.2f} +- {vpp_err:.2f}')
-        return vpp_mean, vpp_err, vrms_mean, vpps, vrms
 
-    def fit_vpp_SG2LAB4D(self, amps_SG, snr_mean, snr_err, vpp, vpp_err, vrms_mean, snr_pure_noise, dic):
+    def fit_vpp_SG2LAB4D(self, amps_SG, dic):
         amps_SG = np.array(amps_SG)
+        snr_mean = np.array([dic[key]["snr_mean"] for key in dic], dtype=float)
+
+        mask = ~np.isnan(snr_mean)  # None in float array is a nan
+        snr_err = np.array([dic[key]["snr_err"] for key in dic])[mask]
+
         try:
             popt, pcov = curve_fit(
-                lin_func, amps_SG, snr_mean, sigma=snr_err, p0=[0.1,5],  absolute_sigma=True)
+                lin_func, amps_SG, snr_mean, sigma=snr_err, p0=[0.1, 5], absolute_sigma=True)
             print('popt', popt)
             pcov = pcov.tolist()
             print('pcov', pcov)
             residuals = snr_mean - lin_func(amps_SG, *popt)
             max_residual = np.max(np.abs(residuals))
             print('max_residual', max_residual)
-        except:
+        except RuntimeError:
             popt = [None, None]
             pcov = None
             max_residual = None
 
         dic_out = {
-            'snr_mean': snr_mean,
-            'snr_err': snr_err,
-            'snr_pure_noise_mean': snr_pure_noise,
-            'vpp_mean': vpp,
-            'vpp_mean_err': vpp_err,
+            'raw_data': dic,
             'amp_SG': list(amps_SG),
-            'vrms_mean': vrms_mean,
             'fit_parameter': {
                 "slope": popt[0],
                 "intercept": popt[1],
                 "pcov": pcov,
                 "max_residual": max_residual
-            },
-            'raw_data': dic
+            }
         }
+
         return dic_out
 
     def eval_fit_result(self, channel, data):
         passed = False
 
-        def check_param(param_value, param_min, param_max):
-            if param_value is None:
-                return False
-            elif param_value < param_min or param_value > param_max:
-                return False
-            return True
+        exp_v = self.conf['expected_values']
 
-        slope_passed = check_param(data['fit_parameter']['slope'], self.conf['expected_values']
-                                   ['slope_min'], self.conf['expected_values']['slope_max'])
-        intercept_passed = check_param(data['fit_parameter']['intercept'], self.conf['expected_values']
-                                       ['intercept_min'], self.conf['expected_values']['intercept_max'])
-        max_residual_passed = check_param(data['fit_parameter']['max_residual'], self.conf['expected_values']
-                                       ['max_residual_min'], self.conf['expected_values']['max_residual_max'])
+        slope_passed = check_param(data['fit_parameter']['slope'],
+                                   exp_v['slope_min'], exp_v['slope_max'])
+        intercept_passed = check_param(data['fit_parameter']['intercept'],
+                                   exp_v['intercept_min'], exp_v['intercept_max'])
+        max_residual_passed = check_param(data['fit_parameter']['max_residual'],
+                                   exp_v['max_residual_min'], exp_v['max_residual_max'])
+
         passed = slope_passed and intercept_passed and max_residual_passed
 
         data['fit_parameter']['res_slope'] = slope_passed
         data['fit_parameter']['res_intercept'] = intercept_passed
         data['fit_parameter']['res_max_residual'] = max_residual_passed
-        print('Test passed:', passed)
-        data = make_serializable(data)
-        self.add_measurement(f"{channel}", data, passed)
-        return data
+        self.logger.info('Test passed:', passed)
+
+        return passed
 
     def run(self):
         super(SignalGen2LAB4D, self).run()
         # turn on the surface amp
         self.device.surface_amps_power_on()
 
-        for i_ch,  ch_radiant in enumerate(self.conf["args"]["channels"]):
+        for i_ch, ch_radiant in enumerate(self.conf["args"]["channels"]):
             logging.info(f"Testing channel {ch_radiant}")
-            print(f"Testing channel {ch_radiant}")
+
             if self.conf["args"]["channel_setting_manual"]:
                 sg_ch, sg_ch_clock, ch_radiant_clock = self.get_channel_settings(ch_radiant, arduino=False)
-                
-                print(f'SigGen channel {sg_ch} --> radiant channel {ch_radiant}')
-                confirmation_signal = None
-                while confirmation_signal != "":
-                    try:
-                        confirmation_signal = input("Press Enter to confirm: ")
-                    except KeyboardInterrupt:
-                        print("Keyboard interrupt. Exiting...")
-                        sys.exit(1)
 
-                print("Confirmed! Signal channel connected.")
-                
-                print(f'SigGen channel {sg_ch_clock} --> radiant channel {ch_radiant_clock}')
-                confirmation_clock = None
-                while confirmation_clock != "":
-                    try:
-                        confirmation_clock = input("Press Enter to confirm: ")
-                    except KeyboardInterrupt:
-                        print("Keyboard interrupt. Exiting...")
-                        sys.exit(1)
+                self.logger.info(f'SigGen channel {sg_ch} --> radiant channel {ch_radiant}')
+                confirm_or_abort()
+                self.logger.info("Confirmed! Signal channel connected.")
 
-                print("Confirmed! Clock channel connected.")
+                self.logger.info(f'SigGen channel {sg_ch_clock} --> radiant channel {ch_radiant_clock}')
+                confirm_or_abort()
+                self.logger.info("Confirmed! Clock channel connected.")
+
             else:
                 sg_ch, sg_ch_clock, ch_radiant_clock = self.get_channel_settings(ch_radiant, arduino=True)
 
-            measured_vpps = []
-            measured_errs = []
-            measured_vrms = []
-            measured_snr = []
-            measured_snr_err = []
-            measured_snr_pure_noise = []
             amps_SG = self.conf['args']['amplitudes']
             ch_dic = {}
             for n, amp_pp in enumerate(amps_SG):
                 print(f"Testing amplitude {amp_pp}")
                 key_str = f'{n}'
-                ch_dic[key_str] = {}
-                ch_dic[key_str]['amp'] = float(amp_pp)  
-                self.awg.set_arb_waveform_amplitude_couple(self.conf['args']['waveform'], 
-                                            sg_ch, 
-                                            sg_ch_clock, 
-                                            amp_pp, 
-                                            self.conf['args']['clock_amplitude'])
+                ch_dic[key_str] = defaultdict(None)
+                ch_dic[key_str]['amp'] = float(amp_pp)
+
+                self.awg.set_arb_waveform_amplitude_couple(
+                    self.conf['args']['waveform'], sg_ch, sg_ch_clock, amp_pp,
+                    self.conf['args']['clock_amplitude'])
+
                 #self.awg.set_frequency_MHz(sg_ch, self.conf['args']['sg_trigger_rate'])
-                self.initialize_config(ch_radiant_clock, self.conf['args']['threshold'])
-                root_file = self.data_dir/"combined.root"
-                if os.path.exists(root_file):
-                    file_size = os.path.getsize(root_file)
-                    file_size_kb = file_size / 1024
-                    if file_size_kb < 5:
+                run = self.initialize_config(ch_radiant_clock, self.conf['args']['threshold'])
+
+                self.logger.info('Start run ....')
+                self.data_dir = self.start_run(run.run_conf, delete_src=True)
+                self.logger.info(f'Stored run at {self.data_dir}')
+
+                wfs_file = self.data_dir / "wfs.dat"
+                if os.path.exists(wfs_file):
+
+                    if os.path.getsize(wfs_file) / 1024 < 5:  # kB
                         logging.warning('File too small, probably no trigger')
-                        ch_dic[key_str]['vpp_mean'] = None
-                        ch_dic[key_str]['vpp_err'] = None
-                        ch_dic[key_str]['vrms_mean'] = None
-                        ch_dic[key_str]['snr_mean'] = None
-                        ch_dic[key_str]['snr_err'] = None
-                        ch_dic[key_str]['snr_pure_noise_mean'] = None
-                        ch_dic[key_str]['vpps'] = None
-                        ch_dic[key_str]['vrms'] = None
-                        ch_dic[key_str]['snrs'] = None
-                        ch_dic[key_str]['run'] = str(root_file)
+                        ch_dic[key_str]['run'] = str(wfs_file)
                     else:
-                        vpp_mean, vpp_err, vrms_mean, snr_mean, snr_err, snr_pure_noise_mean, vpps, vrms, snrs, root_file = self.get_vpp(
-                            root_file, ch_radiant, ch_radiant_clock, amp_pp, key_str)
-                        measured_vpps.append(vpp_mean)
-                        measured_errs.append(vpp_err)
-                        measured_vrms.append(vrms_mean)
-                        measured_snr.append(snr_mean)
-                        measured_snr_err.append(snr_err)
-                        measured_snr_pure_noise.append(snr_pure_noise_mean)
+
+                        vpp_mean, vpp_err, vrms_mean, snr_mean, snr_err, snr_pure_noise_mean, \
+                            vpps, vrms, snrs, file_name = self.get_vpp(
+                                ch_radiant, ch_radiant_clock, amp_pp, key_str)
+
                         ch_dic[key_str]['vpp_mean'] = vpp_mean
                         ch_dic[key_str]['vpp_err'] = vpp_err
                         ch_dic[key_str]['vrms_mean'] = vrms_mean
@@ -345,12 +323,17 @@ class SignalGen2LAB4D(radiant_test.RADIANTTest):
                         ch_dic[key_str]['snrs'] = snrs
                         ch_dic[key_str]['vpps'] = vpps
                         ch_dic[key_str]['vrms'] = vrms
-                        ch_dic[key_str]['run'] = str(root_file)
-            
+                        ch_dic[key_str]['run'] = str(file_name)
+
             dic_out = self.fit_vpp_SG2LAB4D(
-                amps_SG, measured_snr, measured_snr_err, measured_vpps, measured_errs, measured_vrms, measured_snr_pure_noise, ch_dic)
-            data_buffer = self.eval_fit_result(ch_radiant, dic_out)
-            with open(f'/scratch/rno-g/radiant_data/SignalGen2LAB4D_buffer.json', 'w') as f:
+                amps_SG, ch_dic)
+
+            passed = self.eval_fit_result(ch_radiant, dic_out)
+            data_buffer = make_serializable(dic_out)
+            self.add_measurement(f"{ch_radiant}", data_buffer, passed=passed)
+
+            # This is necessary that some following tests (AUXTriggerResponse) can use the data!
+            with open('/scratch/rno-g/radiant_data/SignalGen2LAB4D_buffer.json', 'w') as f:
                 json.dump(data_buffer, f)
 
         # turn off the surface amp
@@ -359,4 +342,3 @@ class SignalGen2LAB4D(radiant_test.RADIANTTest):
 
 if __name__ == "__main__":
     radiant_test.run(SignalGen2LAB4D)
-
