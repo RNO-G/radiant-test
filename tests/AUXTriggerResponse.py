487db41977e129c49dab22623bdbdd556995b38f
Felix Schlüter <felix.schluter@icecube.wisc.edu>
2024-01-23 22:38:05 +0100

    Some more cleanup refactoring

--- tests/AUXTriggerResponse.py
+++ tests/AUXTriggerResponse.py
@@ -4,15 +4,11 @@ import stationrc.remote_control
 import numpy as np
 import uproot
 from scipy.optimize import curve_fit
-import matplotlib.pyplot as plt
 import logging
 import os
 import re
 import json
-import time
-import pathlib
 from radiant_test.radiant_helper import uid_to_name
-import sys
 from radiant_test.util import confirm_or_abort
 
 def hill_eq(x, x0, p):
@@ -40,7 +36,8 @@ class AUXTriggerResponse(radiant_test.SigGenTest):
         result_path = 'results/'
         ulb_id = uid_to_name(self.result_dict['dut_uid'])
         # Get a list of files in the directory
-        files = [os.path.join(result_path, file) for file in os.listdir(result_path) if re.search('SignalGen2LAB4D', file) and file.endswith('.json') and re.search(ulb_id, file)]
+        files = [os.path.join(result_path, file) for file in os.listdir(result_path)
+                 if re.search('SignalGen2LAB4D', file) and file.endswith('.json') and re.search(ulb_id, file)]
         # Find the newest file based on modification time
         newest_file = max(files, key=os.path.getmtime)
 
@@ -48,7 +45,8 @@ class AUXTriggerResponse(radiant_test.SigGenTest):
             result_dict = json.load(f)
 
         def amplitude_conversion(x):
-            return x*result_dict['run']['measurements'][str(channel)]['measured_value']['fit_parameter']['slope'] + result_dict['run']['measurements'][str(channel)]['measured_value']['fit_parameter']['intercept']
+            return x * result_dict['run']['measurements'][str(channel)]['measured_value']['fit_parameter']['slope'] + \
+                result_dict['run']['measurements'][str(channel)]['measured_value']['fit_parameter']['intercept']
 
         return amplitude_conversion
 
@@ -174,7 +172,9 @@ class AUXTriggerResponse(radiant_test.SigGenTest):
             trig_effs.append(sorted_curve_dic[key]['trig_eff'])
             trig_effs_err.append(sorted_curve_dic[key]['trig_eff_err'])
         try:
-            popt, pcov = curve_fit(tanh_func, vpp, trig_effs, sigma=trig_effs_err, p0=self.conf['args']['initial_guess_fit']) #, bounds=([0, 0], [np.inf, np.inf]))#, p0=[100, 2])
+            popt, pcov = curve_fit(
+                tanh_func, vpp, trig_effs, sigma=trig_effs_err,
+                p0=self.conf['args']['initial_guess_fit']) #, bounds=([0, 0], [np.inf, np.inf]))#, p0=[100, 2])
             # popt, pcov = curve_fit(hill_eq, vpp, trig_effs, bounds=([0, 0], [np.inf, np.inf]))#, p0=[100, 2])
             pcov = pcov.tolist()
         except:
@@ -182,36 +182,40 @@ class AUXTriggerResponse(radiant_test.SigGenTest):
             popt = [None, None, None]
             pcov = None
 
-        dic_out = {'Vpp': vpp, 'trigger_effs': trig_effs, 'trigger_effs_err': trig_effs_err, 'fit_parameter': {
-                "halfway": popt[0],
-                "steepness": popt[1],
+        dic_out = {
+            'Vpp': vpp, 'trigger_effs': trig_effs, 'trigger_effs_err': trig_effs_err,
+            'fit_parameter': {
+                "halfway": popt[0], "steepness": popt[1],
                 # "scaling": popt[0],
-                "pcov": pcov}, 'raw_data': sorted_curve_dic}
+                "pcov": pcov},
+            'raw_data': sorted_curve_dic}
+
         return dic_out
 
-    def eval_curve_results(self, channel, data):
-        passed = False
+    def eval_curve_results(self, data):
+
         def check_param(param_value, param_min, param_max):
             if param_value is None:
-                print(param_value, 'is None')
                 return False
-            elif param_value == self.conf['args']['initial_guess_fit'][0] or param_value == self.conf['args']['initial_guess_fit'][1]:
-                print(param_value, 'is equal to intital guess')
+            elif (param_value == self.conf['args']['initial_guess_fit'][0] or
+                  param_value == self.conf['args']['initial_guess_fit'][1]):
                 return False
-            elif param_value < param_min or param_value > param_max:
-                print(param_value, f'not in range ({param_min}, {param_max})')
+            elif not param_min < param_value < param_max:
                 return False
+
             return True
 
-        hor_passed = check_param(data['fit_parameter']['halfway'], self.conf['expected_values']['halfway_min'], self.conf['expected_values']['halfway_max'])
-        steep_passed = check_param(data['fit_parameter']['steepness'], self.conf['expected_values']['steepness_min'], self.conf['expected_values']['steepness_max'])
-        passed = hor_passed and steep_passed
+        hor_passed = check_param(data['fit_parameter']['halfway'],
+                                 self.conf['expected_values']['halfway_min'],
+                                 self.conf['expected_values']['halfway_max'])
 
+        steep_passed = check_param(data['fit_parameter']['steepness'],
+                                   self.conf['expected_values']['steepness_min'],
+                                   self.conf['expected_values']['steepness_max'])
         data['fit_parameter']['res_halfway'] = hor_passed
         data['fit_parameter']['res_steepness'] = steep_passed
-        # print('Test passed:', passed)
-        self.add_measurement(f"{channel}", data, passed)
-        return data
+
+        return hor_passed and steep_passed
 
     def run(self, use_arduino=True):
         super(AUXTriggerResponse, self).run()
@@ -230,7 +234,7 @@ class AUXTriggerResponse(radiant_test.SigGenTest):
                 confirm_or_abort()
                 print("Confirmed! Signal channel connected.")
 
-                print(f'SigGen channel {sg_ch_clock} --> radiant channel {ch_radiant_clock}')
+                print(f'Clock: SigGen channel {sg_ch_clock} --> radiant channel {ch_radiant_clock}')
                 confirm_or_abort()
 
                 print("Confirmed! Clock channel connected.")
@@ -260,7 +264,8 @@ class AUXTriggerResponse(radiant_test.SigGenTest):
                 vpp_str = f"{vpp:.2f}"
                 self.dic_curve[vpp_str] = {}
 
-                run_length = self.conf["args"]["number_of_events"] * 1/self.conf["args"]["sg_trigger_rate"] + 20 # 2 buffer seconds
+                run_length = self.conf["args"]["number_of_events"] * \
+                    1 / self.conf["args"]["sg_trigger_rate"] + 20 # 2 buffer seconds
 
                 run = self.initialize_config(ch_radiant, thresh,
                                              run_length=run_length,
@@ -279,7 +284,6 @@ class AUXTriggerResponse(radiant_test.SigGenTest):
                 stationrc.common.rootify(
                     self.data_dir, self.device.station_conf["daq"]["mattak_directory"])
 
-
                 root_file_channel_trigger = self.data_dir / "combined.root"
                 trig_eff_point, trig_eff_err = self.calc_trigger_eff_points(
                     root_file_channel_trigger, ch_radiant, ch_radiant_clock)
@@ -289,9 +293,11 @@ class AUXTriggerResponse(radiant_test.SigGenTest):
                 self.dic_curve[vpp_str]['sg_amp'] = sg_current_amp
 
             dic_out = self.fit_trigger_curve(self.dic_curve)
-            data_buffer = self.eval_curve_results(ch_radiant, dic_out)
-            with open('/scratch/rno-g/radiant_data/AUXTrigger_Response_buffer.json', 'w') as f:
-                json.dump(data_buffer, f)
+            passed = self.eval_curve_results(dic_out)
+            self.add_measurement(f"{ch_radiant}", dic_out, passed)
+
+            # with open('/scratch/rno-g/radiant_data/AUXTrigger_Response_buffer.json', 'w') as f:
+            #     json.dump(data_buffer, f)
 
         self.awg.output_off(sg_ch)
         self.awg.output_off(sg_ch_clock)
