cbbcffb3d2a6361971c6afc1db95cc0e98911228
Felix Schlüter <felix.schluter@icecube.wisc.edu>
2024-01-23 18:57:06 +0100

    Update ArduinoNano.py according to Lilly's suggestion

--- radiant_test/ArduinoNano.py
+++ radiant_test/ArduinoNano.py
@@ -1,31 +1,51 @@
 import serial
 import logging
 from serial.tools import list_ports
+
+
 class ArduinoNano():
     def __init__(self):
         try:
-            # self.dev = serial.Serial('/dev/cu.usbserial-A104WN3J', 9600, timeout=1)  # mac
-            self.dev = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)  # linux
-            # self.dev = serial.Serial('COM3', 9600, timeout=1)  # windows
+            self.dev = self.open_serial_port(port='/dev/ttyUSB0') # linux
+            #self.dev = self.open_serial_port(port='/dev/cu.usbserial-AI02POV4') # mac
+            #self.dev = self.open_serial_port(port='COM3') # windows
+
         except serial.SerialException as e:
             print(f"Arduino not connected: {e}, chose from this ports")
             print('\n'.join([p.device for p in list(list_ports.comports())]))
 
+    def open_serial_port(self, port='/dev/ttyUSB0', baudrate=9600):
+        return serial.Serial(port, baudrate, timeout=1)
+
+    def close_serial_port(self):
+        if self.dev.is_open:
+            self.dev.close()
 
     def check_ports(self):
         port = list(list_ports.comports())
         for p in port:
             print(p.device)
 
-    def route_signal_to_channel(self, channel):
-        line=None
+    def routing(self, channel):
+        line = None
         line_counter = 0
-        while line != f'Use channel: {channel}' and line_counter<10:
-          self.dev.write(bytes([channel]))
-          line = self.dev.readline().decode('ascii').strip()
-          logging.debug(f"Arduino is routing the signal to {channel}.")
-          line_counter += 1
+        while line != f'Use channel: {channel}' and line_counter < 10:
+            self.dev.write(bytes([channel]))
+            line = self.dev.readline().decode('ascii').strip()
+            logging.debug(f"Arduino is routing the signal to {channel}.")
+            line_counter += 1
         print(f'Tried {line_counter} times to reroute signal; result: {line}')
+        return line
+
+    def route_signal_to_channel(self, channel):
+        try:
+            line = self.routing(channel)
+        except serial.SerialException as e:
+            print(f"Caught SerialException: {e}, reconnect serial port")
+            self.close_serial_port()
+            self.open_serial_port()
+            line = self.routing(channel)
+
         return line
 
 
@@ -49,7 +69,7 @@ struct State {
 
 State states[24] = {
   // RF1
-  {LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW},     // State 0, RF1 
+  {LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW},     // State 0, RF1
   {LOW, LOW, LOW, HIGH, LOW, LOW, HIGH, LOW, LOW, HIGH, LOW, LOW},    // State 1, RF2
   {LOW, LOW, LOW, LOW, HIGH, LOW, LOW, HIGH, LOW, LOW, HIGH, LOW},    // State 2, RF3
   {LOW, LOW, LOW, HIGH, HIGH, LOW, HIGH, HIGH, LOW, HIGH, HIGH, LOW},   // State 3, RF4
@@ -83,8 +103,8 @@ void setup() {
   Serial.begin(9600);  // Initialize the serial communication
 
   pinMode(3, OUTPUT);
-  pinMode(4, OUTPUT); 
-  pinMode(5, OUTPUT);  
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
   pinMode(6, OUTPUT);
   pinMode(7, OUTPUT);
   pinMode(8, OUTPUT);
