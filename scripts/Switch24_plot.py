e3bac94641862700e0079d6baff962305f9b9322
Felix Schlüter <felix.schluter@icecube.wisc.edu>
2024-01-24 19:08:36 +0100

    Introduce Switch24_plot.py

---
+++ scripts/Switch24_plot.py
@@ -0,0 +1,61 @@
+import numpy as np
+from matplotlib import pyplot as plt
+from radiant_test.radiant_helper import uid_to_name
+import argparse
+import json
+
+
+def plot_channel(ax, data, ch, window_label):
+    data_ch = data["run"]["measurements"][f"{ch}"]["measured_value"]
+    y = np.asarray(data_ch[f"waveform_{window_label}"])
+    x = np.arange(len(y)) / (data['radiant_sample_rate']/1000)
+    if ch == data["config"]["args"]["radiant_clock_channel"]:
+        ax.plot(x, y, label=f"ch {ch}, no switch", lw=1)
+    else:
+        ax.plot(x, y, label=f"ch {ch}", lw=1)
+    ax.plot(
+        x,
+        data_ch[f"fit_amplitude_{window_label}"]
+        * np.sin(
+            2 * np.pi * data_ch[f"fit_frequency_{window_label}"] * 1e-3 * x + data_ch[f"fit_phase_{window_label}"]
+        )  # convert frequency to GHz
+        + data_ch[f"fit_offset_{window_label}"],
+        "--", lw=1
+    )
+    ax.legend(loc="upper right")
+
+def plot_all(data):
+    # Plot to PDF
+    ulb_id = uid_to_name(data['dut_uid'])
+    fname = f'{ulb_id}_Switch24_{data["config"]["args"]["frequency"]}MHz.pdf'
+
+    nrows = 4
+    ncols = 6
+
+    # Plot to screen
+    fig2, axs = plt.subplots(nrows=nrows, ncols=ncols, sharex=True, figsize=(6 * ncols, 5 * nrows))
+    idx = 0
+    for wl in ['lower_buffer', 'higher_buffer']:
+        for idx, ch in enumerate(range(24)):
+            ax = axs.flatten()[idx]
+            plot_channel(ax, data, ch, wl)
+            idx += 1
+            ax.set_ylabel("voltage / ADC counts")
+        for ax in axs.T:
+            ax[-1].set_xlabel("time / ns")
+
+    fig2.tight_layout()
+    plt.savefig(fname.replace(".pdf", "_all_channels.pdf"))
+    return fig2
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input", help="input JSON file")
+    parser.add_argument("-c", "--channel", type=int, nargs="*", help="only plot single channel")
+    parser.add_argument("-w", "--web", action="store_true", help="Return figures to be displayed in web")
+    args = parser.parse_args()
+
+    with open(args.input, "r") as f:
+        data = json.load(f)
+
+    plot_all(data)
--- tests/Switch24.py
+++ tests/Switch24.py
@@ -41,8 +41,6 @@ class Switch24(radiant_test.SigGenTest):
         self.awg.output_off(1)
         self.awg.output_off(2)
 
-        self.plot_all(self.result_dict)
-        plt.show()
 
     def run_channel(self, channel):
         # take some sine waves -> fit the sine waves
@@ -130,67 +128,28 @@ class Switch24(radiant_test.SigGenTest):
             except ValueError:
                 popt = [0, 0, 0, 0]
                 avg_residual = 0
+
             data[f"waveform_{window_label[iwvf]}"] = wvf
             data[f"fit_amplitude_{window_label[iwvf]}"] = (
                 popt[0] if popt[0] >= 0 else -popt[0]
             )  # ensure amplitude is not negative
+
             data[f"fit_frequency_{window_label[iwvf]}"] = popt[1] * 1e3  # convert from GHz to MHz
             data[f"fit_phase_{window_label[iwvf]}"] = (
                 popt[2] if popt[0] >= 0 else popt[2] + np.pi
             )  # correct phase by pi if amplitude was fit negative
+
             # Normalize phase to [0, 2pi)
             while data[f"fit_phase_{window_label[iwvf]}"] < 0:
                 data[f"fit_phase_{window_label[iwvf]}"] += 2 * np.pi
+
             while data[f"fit_phase_{window_label[iwvf]}"] >= 2 * np.pi:
                 data[f"fit_phase_{window_label[iwvf]}"] -= 2 * np.pi
+
             data[f"fit_offset_{window_label[iwvf]}"] = popt[3]
             data[f"fit_avg_residual_{window_label[iwvf]}"] = avg_residual
 
         return data
-
-    def plot_channel(self, ax, data, ch, window_label):
-        data_ch = data["run"]["measurements"][f"{ch}"]["measured_value"]
-        y = np.asarray(data_ch[f"waveform_{window_label}"])
-        x = np.arange(len(y)) / (data['radiant_sample_rate']/1000)
-        if ch == self.conf["args"]["radiant_clock_channel"]:
-            ax.plot(x, y, label=f"ch {ch}, no switch", lw=1)
-        else:
-            ax.plot(x, y, label=f"ch {ch}", lw=1)
-        ax.plot(
-            x,
-            data_ch[f"fit_amplitude_{window_label}"]
-            * np.sin(
-                2 * np.pi * data_ch[f"fit_frequency_{window_label}"] * 1e-3 * x + data_ch[f"fit_phase_{window_label}"]
-            )  # convert frequency to GHz
-            + data_ch[f"fit_offset_{window_label}"],
-            "--", lw=1
-        )
-        ax.legend(loc="upper right")
-
-    def plot_all(self, data):
-        # Plot to PDF
-        ulb_id = uid_to_name(self.result_dict['dut_uid'])
-        fname += f'{ulb_id}_Switch24_{self.config["args"]["frequency"]}MHz.pdf'
-
-        nrows = 4
-        ncols = 6
-
-        # Plot to screen
-        fig2, axs = plt.subplots(nrows=nrows, ncols=ncols, sharex=True, figsize=(6 * ncols, 5 * nrows))
-        idx = 0
-        for wl in ['lower_buffer', 'higher_buffer']:
-            for ch in range(24):
-                ax = axs[idx // ncols][idx % ncols]
-                self.plot_channel(ax, data, ch, wl)
-                idx += 1
-                ax.set_ylabel("voltage / ADC counts")
-            for ax in axs.T:
-                ax[-1].set_xlabel("time / ns")
-
-        fig2.tight_layout()
-        plt.savefig(fname.replace(".pdf", "_all_channels.pdf"))
-        return fig2
-
 
 if __name__ == "__main__":
     radiant_test.run(Switch24)
