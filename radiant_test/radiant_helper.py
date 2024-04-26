0577020269bcafc1f25574a73440e26be1f82f49
Felix Schlüter <felix.schluter@icecube.wisc.edu>
2024-04-26 17:52:45 -0500

    Remove mongo db. fix bug

--- radiant_test/radiant_helper.py
+++ radiant_test/radiant_helper.py
@@ -1,5 +1,4 @@
 import stationrc.remote_control
-from pymongo import MongoClient
 import logging
 
 
@@ -9,6 +8,23 @@ RADIANT_SAMPLING_RATE = 3.2  # GHz
 
 RADIANT = None
 
+uid_to_name_dict = {
+    "e7e318ffb2ad88e35055334a3b82ba18": "ULB-002",
+    "e7e318ffb2ad88e350533357e0ea06e7": "ULB-003",
+    "e7e318ffb2ad88e35055334abd80f19d": "ULB-004",
+    "e7e318ffb2ad88e35055334a2285c7bf": "ULB-005",
+    "e7e318ffb2ad88e35055334abf77d357": "ULB-006",
+    "e7e318ffb2ad88e35055334a96940be5": "ULB-007",
+    "e7e318ffb2ad88e35055334aa54b5690": "ULB-008",
+    "e7e318ffb2ad88e35055334a9ec6c19a": "ULB-009",
+    "e7e318ffb2ad88e35055334a23a5fbc5": "ULB-011",
+    "e7e318ffb2ad88e35055334af865436c": "ULB-012",
+    "e7e318ffb2ad88e35055334ab599703d": "ULB-013",
+    "e7e318ffb2ad88e35055334abf07b869": "ULB-014",
+    "e7e318ffb2ad88e35053335737bcc1da": "ULB-015",
+    "e7e318ffb2ad88e35055334accd821d2": "ULB-016",
+    "e7e318ffb2ad88e35055334a98d0ae04": "ULB-017",
+}
 
 def get_channels_for_quad(quad):
     if quad == 0:
@@ -38,40 +54,14 @@ def get_radiant():
         RADIANT = stationrc.remote_control.VirtualStation()
     return RADIANT
 
-def get_mongo_database():
-    mongo_conf = {
-        "hostname": "radio.zeuthen.desy.de",
-        "port": 27017,
-        "username": "read",
-        "password": "EseNbGVaCV4pBBrt",
-    }
-    mongo_uri = f"mongodb://{mongo_conf['username']}:{mongo_conf['password']}@{mongo_conf['hostname']}:{mongo_conf['port']}/admin?authSource=admin&directConnection=true&ssl=true"
-
-    mongo_client = MongoClient(mongo_uri)
-    mongo_db = mongo_client["RNOG_live"]
-    return mongo_db
 
 def uid_to_name(uid):
-    try:
-        mongo_db = get_mongo_database()
-    except:
-        return uid
-
-    search_filter = [{'$match': {'dut_id': uid}}]
-    search_result = list(mongo_db['radiant_dut_id'].aggregate(search_filter))
-
-    if len(search_result) == 0:
-        logging.error(f"Name for {uid} unknown")
-        return uid
-    elif len(search_result) > 1:
-        logging.error(f"More than one name found for {uid}")
-        return uid
-    else:
-        return search_result[0]['ulb_id']
-
+    if uid in uid_to_name_dict:
+        return uid_to_name_dict[uid]
+    return uid
 
 def get_key_for_sampling_rate(dic, key, sr):
-    radiant_sample_rate = "2G4" if self.result_dict["radiant_sample_rate"] == 2400 else "3G2"
+    radiant_sample_rate = "2G4" if dic["radiant_sample_rate"] == 2400 else "3G2"
     key2 = f"{key}_{radiant_sample_rate}"
     if key2 in dic:
         return dic[key2]
